07e51f23dd0e1ee0a43f0b696a6f49b8b461e203
ColinZeidler <c.zeidler.wc@gmail.com>
2013-07-05 20:38:40 -0400

    now draws a filed of snowmen
    you can also move and look around with the arrow keys

--- openGL Test/openGL Test/Source.cpp
+++ openGL Test/openGL Test/Source.cpp
@@ -1,4 +1,5 @@
 #include <GL/glew.h>
+#include <math.h>
 
 #ifdef __APPLE__
 #include <GLUT/glut.h> //allows crossplatform with apple
@@ -7,15 +8,26 @@
 #endif
 
 void renderScene(void);
+void drawSnowMan(void);
 void changeSize(int, int);
 void normalKeys(unsigned char, int, int);
-void specialKeys(int, int, int);
+void pressKey(int, int, int);
+void releaseKey(int, int, int);
 
-float angle = 0.0f;
+void computePos(float);
+void computeDir(float);
 
-float red = 1.0f;
-float green = 1.0f;
-float blue = 1.0f;
+// angle of rotation for the camera direction
+float angle=0.0;
+// actual vector representing the camera's direction
+float lx=0.0f,lz=-1.0f;
+// XZ position of the camera
+float x=0.0f,z=5.0f;
+
+// the key states. These variables will be zero
+//when no key is being presses
+float deltaAngle = 0.0f;
+float deltaMove = 0;
 
 int main(int argc, char **argv) {
 
@@ -31,10 +43,17 @@ int main(int argc, char **argv) {
 
 	glutReshapeFunc(changeSize);
 
-	glutIdleFunc(renderScene);	//cpu use is astronomical, should figure out how to reduce the number of renders performed...
-
+	glutIdleFunc(renderScene);
 	glutKeyboardFunc(normalKeys);
-	glutSpecialFunc(specialKeys);
+	glutSpecialFunc(pressKey);
+
+	
+	// here are the new entries
+	glutIgnoreKeyRepeat(1);
+	glutSpecialUpFunc(releaseKey);
+	
+	// OpenGL init
+	glEnable(GL_DEPTH_TEST);
 
 	//begin event processing cycle
 	glutMainLoop();
@@ -43,26 +62,40 @@ int main(int argc, char **argv) {
 }
 
 void renderScene(void) {
+
+	if (deltaMove)
+		computePos(deltaMove);
+	if (deltaAngle)
+		computeDir(deltaAngle);
+
+	// Clear Color and Depth Buffers
+
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	//reset transformations
+	// Reset transformations
 	glLoadIdentity();
-	//set the camera
-	gluLookAt(	0.0f, 0.0f, 10.0f,
-				0.0f, 0.0f, 0.0f,
-				0.0f, 1.0f, 0.0f);
+	// Set the camera
+	gluLookAt(	x, 1.0f, z,
+			x+lx, 1.0f,  z+lz,
+			0.0f, 1.0f,  0.0f);
 
-	glRotatef(angle, 0.0f, 1.0f, 0.0f);
-
-	//set the color of the triangle
-	glColor3f(red,green,blue);
-	glBegin(GL_TRIANGLES);
-		glVertex3f(-2.0f,-2.0f, 0.0f);
-		glVertex3f( 2.0f, 0.0f, 0.0);
-		glVertex3f( 0.0f, 2.0f, 0.0);
+        // Draw ground
+	glColor3f(0.9f, 0.9f, 0.9f);
+	glBegin(GL_QUADS);
+		glVertex3f(-100.0f, 0.0f, -100.0f);
+		glVertex3f(-100.0f, 0.0f,  100.0f);
+		glVertex3f( 100.0f, 0.0f,  100.0f);
+		glVertex3f( 100.0f, 0.0f, -100.0f);
 	glEnd();
 
-	angle +=0.5f;
+        // Draw 36 SnowMen
+	for(int i = -3; i < 3; i++)
+		for(int j=-3; j < 3; j++) {
+			glPushMatrix();
+			glTranslatef(i*10.0,0,j * 10.0);
+			drawSnowMan();
+			glPopMatrix();
+		}
 
 	glutSwapBuffers();
 }
@@ -91,29 +124,64 @@ void changeSize(int width, int height) {
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void normalKeys(unsigned char key, int x, int y) {
+void normalKeys(unsigned char key, int xx, int yy) {
 	if (key == 27)	//excape is pressed
 		exit(0);
 }
 
-void specialKeys(int key, int x, int y) {
+void drawSnowMan() {
 
-	//change the colour of the triangle when a key is pressed
-	switch(key) {
-		case GLUT_KEY_F1:
-			red = 1.0f;
-			green = 0.0f;
-			blue = 0.0f;
-			break;
-		case GLUT_KEY_F2:
-			red = 0.0f;
-			green =1.0f;
-			blue = 0.0f;
-			break;
-		case GLUT_KEY_F3:
-			red = 0.0f;
-			green = 0.0f;
-			blue = 1.0f;
-			break;
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+// Draw Body
+	glTranslatef(0.0f ,0.75f, 0.0f);
+	glutSolidSphere(0.75f,20,20);
+
+// Draw Head
+	glTranslatef(0.0f, 1.0f, 0.0f);
+	glutSolidSphere(0.25f,20,20);
+
+// Draw Eyes
+	glPushMatrix();
+	glColor3f(0.0f,0.0f,0.0f);
+	glTranslatef(0.05f, 0.10f, 0.18f);
+	glutSolidSphere(0.05f,10,10);
+	glTranslatef(-0.1f, 0.0f, 0.0f);
+	glutSolidSphere(0.05f,10,10);
+	glPopMatrix();
+
+// Draw Nose
+	glColor3f(1.0f, 0.5f , 0.5f);
+	glRotatef(0.0f,1.0f, 0.0f, 0.0f);
+	glutSolidCone(0.08f,0.5f,10,2);
+}
+
+void computePos(float deltaMove) {
+	x += deltaMove * lx * 0.1f;
+	z += deltaMove * lz * 0.1f;
+}
+
+void computeDir(float deltaAngle) {
+	angle += deltaAngle;
+	lx = sin(angle);
+	lz = -cos(angle);
+}
+
+void pressKey(int key, int xx, int yy) {
+	switch (key) {
+		case GLUT_KEY_LEFT : deltaAngle = -0.01f; break;	
+		case GLUT_KEY_RIGHT : deltaAngle = 0.01f; break;
+		case GLUT_KEY_UP : deltaMove = 0.5f; break;
+		case GLUT_KEY_DOWN : deltaMove = -0.5f; break;
+	}
+}
+
+void releaseKey(int key, int x, int y) {
+
+	switch (key) {
+		case GLUT_KEY_LEFT :
+		case GLUT_KEY_RIGHT : deltaAngle = 0.0f;break;
+		case GLUT_KEY_UP :
+		case GLUT_KEY_DOWN : deltaMove = 0;break;
 	}
 }
---
+++ openGL Test/openGL Test/readOBJ.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//
+//using namespace std;
+//void main() {
+//	string fileName;
+//	ifstream File;
+//	string line;
+//
+//	cout << "File > basicPerson.obj" << endl;
+//
+//	File.open("C:\\Users\\Colin\\Pictures\\Game designs\\basicPerson.obj");
+//	if (File.is_open()) {
+//		while (File.good()) {
+//			getline(File,line);
+//			cout << line << endl;
+//		}
+//	}
+//
+//	File.close();
+//
+//	cin >>fileName;
+//}
